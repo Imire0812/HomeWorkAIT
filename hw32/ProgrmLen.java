b08d1ceabaef89251fea54a28b568353d833c9f6
Kate Horshkova <poligrafist87@gmail.com>
2023-04-28 22:17:02 +0200

    commit 32hw
    just getting started

---
+++ hw32/ProgrmLen.java
@@ -0,0 +1,14 @@
+package hw32;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProgrmLen {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(new File("res/dict.txt"));
+
+    }
+
+}
